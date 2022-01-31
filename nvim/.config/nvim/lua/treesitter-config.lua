c27839161a92712c67ba658191b56872ac5f37d3
Bharadwaja-rao-D <cs20btech11012@iith.ac.in>
2022-01-31 20:19:33 +0530

    changed wallpapers :

--- nvim/.config/nvim/lua/treesitter-config.lua
+++ nvim/.config/nvim/lua/treesitter-config.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-  ensure_installed = {"c", "cpp", "rust"},
+  ensure_installed = {"c", "cpp", "rust", "java"},
 
   -- Install languages synchronously (only applied to `ensure_installed`)
   sync_install = false,

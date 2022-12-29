e8cafbb58eecb05faf39bbecc540e96dcd64762f
bharadwaja-rao-d <cs20btech11012@iith.ac.in>
2022-12-29 17:30:11 +0530

    added git remap

--- nvim/.config/nvim/after/plugin/remap.lua
+++ nvim/.config/nvim/after/plugin/remap.lua
@@ -15,3 +15,6 @@ vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
+
+vim.keymap.set("n", "<leader>go", ":Git<cr>");
+vim.keymap.set("n", "<leader>gp", ":Git push<cr>");

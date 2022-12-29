5f0b28d49e7b22e7b6a6021cbfad4471e91c529b
bharadwaja-rao-d <cs20btech11012@iith.ac.in>
2022-12-29 17:54:26 +0530

    changed colorscheme

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,3 +1,16 @@
+function OneNordColor()
+    require('onenord').setup({
+        theme = nil, -- "dark" or "light". Alternatively, remove the option and set vim.o.background instead
+        borders = true, -- Split window borders
+        fade_nc = false, -- Fade non-current windows, making them more distinguishable
+        disable = {
+            background = true, -- Disable setting the background color
+            eob_lines = true, -- Hide the end-of-buffer lines
+        },
+    })
+
+end
+
 function ColorIt()
     local color = "tokyonight"
     vim.cmd.colorscheme(color)
@@ -5,4 +18,5 @@ function ColorIt()
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-ColorIt()
+OneNordColor()
+
